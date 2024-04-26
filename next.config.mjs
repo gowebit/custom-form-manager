a94b61835fa204912edb94789d6ba5258be0a0fe
Gabriel Valverde <gabriel_gom@hotmail.com>
2024-04-26 22:19:56 +0100

    chore: add comments on next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // On "next build", outputs the static build to the "out" folder
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 export default nextConfig;
+s;
